b6b0ef8902237cac1cf0fd72f8161de97b211674
abdulbasir11 <abdulbasirmusician@gmail.com>
2020-12-02 05:24:00 -0500

    added (unimplemented) methods

--- Generators/MineralGenerator.cs
+++ Generators/MineralGenerator.cs
@@ -6,38 +6,47 @@ using System.Threading.Tasks;
 
 namespace Bolder_Blacksmith.Generators
 {
-    /* Mineral generator. Generators minerals.
-     * Mineral properties include:
-     *      -Number of minerals. Ranges from 1-4.
-     *      
-     *      -Mineral distribution. Sets distributions of each
-     *      mineral based on covalence and gravity, with some
-     *      random modification.
-     *      
-     *      -Complexity. Minerals with elements with high covalence,
-     *      highly geometric crystalline structures tend to be duller,
-     *      but have extreme colors.
-     *      
-     *      -Net structure. Normalized 0-1 of sum of each structure,
-     *      with 0 being crystalline and 1 being cubic.
-     *      
-     *      -Net deformation. Sum of all deformations.
-     *      
-     *      -Net density. Sum of all densities.
-     *      
-     *      -Net harndess.
-     *      
-     *      -Net pliance.
-     *      
-     *      -Net cleaving tendency
-     *      
-     *      -Net heat resistance
-     *      
-     *      -Net heat hardness ratio
-     * 
+    /* Mineral generator. Generates minerals from initialize() function, which calls all generation methods.
      */
     public class MineralGenerator
     {
+        public Mineral getMineral()
+        {
+            //Mineral min = new Mineral();
+            //min.initialize();
+            //return min;
+            throw new Exception("implement");
+        }
 
+        public Mineral getIsotopicMineral()
+        {
+            //Mineral min = new Mineral();
+            //min.initializeIsotopic();
+            //return min;
+            throw new Exception("implement");
+        }
+
+        public Mineral [] getBatchMinerals(int numberOfMinerals, bool isIsotopic)
+        {
+            /*
+            Mineral[] mins = new Mineral[numberOfMinerals];
+            Mineral holder;
+
+            for (int i = 0; i < mins.Length; i++)
+            {
+                holder = new Mineral();
+                if (isIsotopic){
+                holder.initializeIsotopic();
+                }
+                else{
+                holder.initialize();
+                }
+                mins[i] = holder;
+            }
+
+            return mins
+            */
+            throw new Exception("implement");
+        }
     }
 }

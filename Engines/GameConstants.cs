e60fc66827b9e419775075c8cf21ea057d59403d
abdulbasir11 <abdulbasirmusician@gmail.com>
2020-12-07 00:44:00 -0500

    added constants for GameInstance class

---
+++ Engines/GameConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bolder_Blacksmith.Engines
+{
+    public static class GameConstants
+    {
+        //Game-breaking constants are marked with an "!".
+
+        //Min and max pressure must have same abs value.
+        public static int MINIMUM_PRESSURE = -5;
+        public static int MAXIMUM_PRESSURE = MINIMUM_PRESSURE * -1;
+        public static int PRESSURE_DAMPENER = 1; //!!! Must be >= 1 and < MAXIMUM_PRESSURE!
+
+    }
+}
